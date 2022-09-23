362f434dad36a0fb8276dbecc586770404d7d92e
Kruuux <kruczekkonrad.kk@gmail.com>
2022-09-23 14:08:45 +0200

    feat: apply changes to the function

--- HttpTriggerAddFunc/index.ts
+++ HttpTriggerAddFunc/index.ts
@@ -6,9 +6,15 @@ const httpTrigger: AzureFunction = async function (
 ): Promise<void> {
   context.log("HTTP trigger function processed a request.");
   const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? "Hello, " + name + ". This HTTP triggered function executed successfully!"
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const a = req.query.a || (req.body && req.body.a);
+  const b = req.query.b || (req.body && req.body.b);
+
+  const responseMessage =
+    name && a && b
+      ? `Hello, ${name}. This HTTP triggered function executed successfully! The result of a + b is ${
+          parseInt(a) + parseInt(b)
+        }`
+      : `This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."`;
 
   context.res = {
     status: 200,
